7c707e157bfc73981a294b18a4e0f49dc9b0d894
66RING <1552087953@qq.com>
2020-09-28 22:44:49 +0800

    file CRUD and Doc

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 ## 开发日志/技术总结 
 
-### http包
+### 技术栈
+
+#### 持久化
+    * 不单单保存在内存中，否则每次重启数据都会丢失
+    * 使用数据库(这里使用mySQL)进行文件持久化
+
+    
+#### 集群
+    * 单机搭建集群可以使用docker来启动几个mySQL容器
+
+
+### API使用
+
+#### io
+
+- `io.Create("path")`, 打开一个文件流
+- `io.Copy(newFile, sourceFile) fileSize, error`
+
+
+#### http包
 
 ```go
 func handlerFunc(w http.ResponseWriter, r *http.Request) {}
@@ -12,4 +31,34 @@ err := http.ListenAndServe(":port", nil)
 
 - 使用`io.WriteString(w, "inter err")`往(http)流里写入
 - `r.Method`可查看request类型
+
+
+##### 文件信息查询
+
+`r *http.Request`会包含请求的所有信息，如form表单等
+
+- `r.ParseForm()`解析表单
+    * 通过`r.Form["key"][0]`等操作可以获取表单信息
+
+
+##### 文件下载
+
+想要浏览器识别文件并下，需要设置header，如:
+
+```go
+w.Header().Set("Content-Type", "application/octect-stream")
+w.Header().Set("Content-Disposition", "attachment;filename=\""+fm.FileName+"\"")
+```
+
+
+##### 文件更新删除
+
+- 更新/删除系统中的数据
+    * 删除操作需要考虑线程安全问题
+- 更新/删除内存中的数据
+
+
+#### Time
+
+time.Now().Format("2006-01-02 15:04:05"), go里面一个特殊时间点
 
--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"GoCloud/meta"
+	"GoCloud/util"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,19 +29,30 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		defer file.Close()
-		newfile, err := os.Create("/tmp/" + head.Filename)
+
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"), // go里面一个特殊时间点
+		}
+
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Println("fail to create file, ", err.Error())
 			return
 		}
-		defer newfile.Close()
+		defer newFile.Close()
 
 		// target, source
-		_, err = io.Copy(newfile, file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			fmt.Println("fail to save date, ", err.Error())
 			return
 		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
 
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
@@ -46,4 +61,82 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 // Upload success
 func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Upload finished")
+}
+
+// 获取文件信息
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filehash := r.Form["filehash"][0]
+	fMeta := meta.GetFileMeta(filehash)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fsha1 := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(fsha1)
+
+	f, err := os.Open(fm.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	// 文件很小就直接全部加载到内存了
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/octect-stream")
+	w.Header().Set("Content-Disposition", "attachment;filename=\""+fm.FileName+"\"")
+
+	w.Write(data)
+}
+
+func FileMetaUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	opType := r.Form.Get("op")
+	fileSha1 := r.Form.Get("filehash")
+	newFileName := r.Form.Get("filename")
+
+	if opType != "0" {
+		w.WriteHeader(http.StatusForbidden) // 403
+		return
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed) // 405
+		return
+	}
+
+	curFileMeta := meta.GetFileMeta(fileSha1)
+	curFileMeta.FileName = newFileName
+	meta.UpdateFileMeta(curFileMeta)
+
+	data, err := json.Marshal(curFileMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func FileDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fileSha1 := r.Form.Get("filehash")
+	// 删除先取出位置，在系统中删除
+	fMeta := meta.GetFileMeta(fileSha1)
+	os.Remove(fMeta.Location)
+
+	meta.RemoveFileMeta(fileSha1)
+
+	w.WriteHeader(http.StatusOK)
 }
