6607055e641ef909bbe054eb7cf771edb15617bc
66RING <1552087953@qq.com>
2020-10-01 22:24:44 +0800

    db handler update

--- meta/filemeta.go
+++ meta/filemeta.go
@@ -1,5 +1,9 @@
 package meta
 
+import (
+	mydb "GoCloud/db"
+)
+
 // 文件元信息结构
 type FileMeta struct {
 	FileSha1 string
@@ -20,8 +24,30 @@ func UpdateFileMeta(fmeta FileMeta) {
 	fileMetas[fmeta.FileSha1] = fmeta
 }
 
+// 新增/更新文件元信息到mysql
+func UpdateFileMetaDB(fmeta FileMeta) bool {
+	return mydb.OnFileUploadFinished(
+		fmeta.FileSha1, fmeta.FileName, fmeta.FileSize, fmeta.Location)
+}
+
 func GetFileMeta(fileSha1 string) FileMeta {
 	return fileMetas[fileSha1]
+}
+
+// 从mysql获取文件元信息
+func GetFileMetaDB(fileSha1 string) (FileMeta, error) {
+	tfile, err := mydb.GetFileMeta(fileSha1)
+	if err != nil {
+		return FileMeta{}, err
+	}
+	fmeta := FileMeta{
+		FileSha1: tfile.FileHash,
+		FileName: tfile.FileName.String,
+		FileSize: tfile.FileSize.Int64,
+		Location: tfile.FileAddr.String,
+	}
+
+	return fmeta, nil
 }
 
 func RemoveFileMeta(fileSh1 string) {
